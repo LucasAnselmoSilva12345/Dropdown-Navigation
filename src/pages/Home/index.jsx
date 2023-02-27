2797621cf9dacd1d9d70b6d31a1104c05523413a
LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>
2023-02-27 16:50:45 -0300

    Feat Home content and images in Homepage Feat responsive styles
    Signed-off-by: LucasAnselmoSilva12345 <lucasanselmodasilva02@gmail.com>

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,19 +1,47 @@
 import React from 'react';
 import Header from '../../components/Header';
 
-import { HomeContainer } from './style.js';
+import imageHeroMobile from '../../assets/images/image-hero-mobile.png';
+
+import imageClientAudioPhile from '../../assets/images/client-audiophile.svg';
+import imageClientDatabiz from '../../assets/images/client-databiz.svg';
+import imageClientMaker from '../../assets/images/client-maker.svg';
+import imageClientMeet from '../../assets/images/client-meet.svg';
+
+import {
+  HomeContainer,
+  HomeContent,
+  HomeTitle,
+  HomeDescription,
+  HomeButton,
+  HomeImageContainer,
+  HomeImageWrapper,
+  HomeImage,
+} from './style.js';
 
 export function Home() {
   return (
     <>
       <Header />
       <HomeContainer>
-        <div className="col-1">
-          <h1>Home</h1>
-        </div>
-        <div className="col-2">
-          <h1>Imagem</h1>
-        </div>
+        <HomeContent>
+          <HomeTitle>Make remote work</HomeTitle>
+          <HomeDescription>
+            Get your team in sync, no matter your location. Streamline
+            processes, create team rituals, and watch productivity soar.
+          </HomeDescription>
+          <HomeButton>Learn more</HomeButton>
+
+          <HomeImageContainer>
+            <HomeImageWrapper src={imageClientDatabiz} />
+            <HomeImageWrapper src={imageClientAudioPhile} />
+            <HomeImageWrapper src={imageClientMaker} />
+            <HomeImageWrapper src={imageClientMeet} />
+          </HomeImageContainer>
+        </HomeContent>
+        <section>
+          <HomeImage src={imageHeroMobile} alt="" />
+        </section>
       </HomeContainer>
     </>
   );
